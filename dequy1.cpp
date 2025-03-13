c9685a07b885f93a7afc46c66f988b887b19f77e
chthollywaifu <nguyenconghoang120281@gmail.com>
2025-03-14 00:49:34 +0700

    dequy1

---
+++ dequy1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long F(int n)
+{
+	if(n<=100)
+	return F(F(n+11));
+	else if(n>=101)
+	return n-10;
+}
+int main()
+{
+	int n;
+cin >>  n;
+cout << F(n); //mota2
+}
+
